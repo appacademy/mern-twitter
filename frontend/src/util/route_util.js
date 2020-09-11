292e74b2d9baa69818c6f671fbf9b7db6ba6e0a8
JasonEb <jasonebueng@gmail.com>
2020-09-11 15:28:48 -0700

    rewrites ProtectedRoute

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import { connect, useSelector } from 'react-redux';
-import { Route, Redirect, withRouter } from 'react-router-dom';
+import React from "react";
+import { connect, useSelector } from "react-redux";
+import { Route, Redirect, withRouter } from "react-router-dom";
 
 const Auth = ({ component: Component, path, exact }) => {
-  const loggedIn = useSelector( state => { return state.session.isAuthenticated})
-  
-  return <Route path={path} exact={exact} render={(props) => (
-    !loggedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/tweets" />
-    )
-  )} />
-}
+  const loggedIn = useSelector((state) => state.session.isAuthenticated);
 
-const Protected = ({ component: Component, loggedIn, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      loggedIn ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
-  />
-);
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(props) =>
+        !loggedIn ? <Component {...props} /> : <Redirect to="/tweets" />
+      }
+    />
+  );
+};
 
-const mapStateToProps = state => (
-  {loggedIn: state.session.isAuthenticated}
-);
+const Protected = ({ component: Component, ...rest }) => {
+  const loggedIn = useSelector((state) => state.session.isAuthenticated);
 
-export const AuthRoute = withRouter(Auth)
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        loggedIn ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
 
-export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
+export const AuthRoute = withRouter(Auth);
+
+export const ProtectedRoute = withRouter(Protected);
